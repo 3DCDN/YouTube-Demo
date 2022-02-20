d9353b3943e82a341bfbcaf6f7a7256820caf91d
3DCDN <richst.ongeywg@gmail.com>
2022-02-20 15:44:16 -0600

    First Initial Commit of initial project files.

---
+++ YouTube-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTube-Demo
+//
+//  Created by Richard St. Onge on 2022-02-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
